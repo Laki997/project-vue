0149bab1c2a52af769dc93f8b635670ae77626e9
Lazar Kalajdzic <laki.kalajdzic@gmail.com>
2021-06-21 21:02:00 +0200

    login

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,9 +1,60 @@
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+          v-model="user.email"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          class="form-control"
+          id="password"
+          aria-describedby="emailHelp"
+          placeholder="Enter password"
+          v-model="user.password"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+
+  methods: {
+    ...mapActions({ login: "auth/login" }),
+    async onSubmit() {
+      await this.login(this.user)
+        .then(() => {
+          this.$router.push("/about");
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
